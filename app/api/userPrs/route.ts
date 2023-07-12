ce8bc428e3f919fe0feb95895e9f446a339f2d49
C4PT4INNULL <chaudharyarun5797@gmail.com>
2023-07-12 18:07:50 +0530

    init

--- app/api/userPrs/route.ts
+++ app/api/userPrs/route.ts
@@ -2,14 +2,17 @@
 import fetchUserPulls from '../../services/fetchUserPulls'
 import {processUserPRs} from '../../helpers/usersHelper'
 const { Octokit } = require('@octokit/core')
+const querystring = require('querystring')
 
 export async function GET(req: Request){
     try {
         const octokit = new Octokit({ auth: process.env.GIT_TOKEN })
-        const url = new URL(req.url);
-        const gitUser = url.searchParams.get('gitUser');
-        const fromDate = url.searchParams.get('fromDate');
-        const toDate = url.searchParams.get('toDate');
+        const { search } = new URL(req.url)
+        const { gitUser, fromDate, toDate } = querystring.parse(search.replace('?', ''))
+        // const url = new URL(req.url);
+        // const gitUser = url.searchParams.get('gitUser');
+        // const fromDate = url.searchParams.get('fromDate');
+        // const toDate = url.searchParams.get('toDate');
       const data = await fetchUserPulls({
         startDate: fromDate,
         endDate: toDate,
